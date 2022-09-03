6ef6258b47907362013d491846f5f7ac26800028
Cesar <camongez28@gmail.com>
2022-09-02 23:28:08 -0400

    git init

---
+++ desafio_2/clases.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+
+class Container {
+  constructor(path) {
+    this.path = path   
+  }
+
+  async save(obj) {
+    if (fileExists(this.path)) {
+      try {
+        let products = await this.getAll();
+        let lastId = (await products[products.length - 1].id) + 1;
+        obj.id = lastId;
+        products.push(obj);
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(products, null, 2)
+        );
+        return obj.id;
+      } catch (error) {
+        console.log('Error al guardar el producto');
+      }
+    } else {
+      try {
+        let products = [];
+        obj.id = 1;
+        products.push(obj);
+        fs.promises.writeFile(this.path, JSON.stringify(products, null, 2));
+        return obj.id;
+      } catch (error) {
+        console.log('Error al crear el archivo y guardar el producto');
+      }
+    }
+  }
+
+  async getByID(id) {
+    try {
+      if (fileExists(this.path)) {
+        let products = await this.getAll();
+        let obj = products.find((item) => item.id == id);
+
+        return obj !== undefined ? obj : null;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getAll() {
+    if (fileExists(this.path)) {
+      try {
+        let products = await fs.promises.readFile(this.path, 'utf-8');
+        return JSON.parse(products);
+      } catch (error) {
+        return [];
+      }
+    } else {
+      console.log('El archivo que estas buscando no existe.');
+    }
+  }
+  async deleteById(id) {
+    if (fileExists(this.path)) {
+      try {
+        let products = await this.getAll();
+        let newProducts = products.filter((item) => item.id != id);
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(newProducts, null, 2)
+        );
+        console.log('listo');
+      } catch (error) {
+        console.log('Error al borrar el producto');
+      }
+    }
+  }
+
+  async deleteAll() {
+    if (fileExists(this.path)) {
+      try {
+        await fs.promises.unlink(this.path);
+      } catch (error) {
+        console.log('Error al borrar el contenido del archivo');
+      }
+    } else {
+      console.log('El archivo no existe');
+    }
+  }
+}
+//helpers
+
+const fileExists = (path) => {
+  try {
+    return fs.statSync(path).isFile();
+  } catch (err) {
+    return false;
+  }
+};
+
+module.exports = Container;
---
+++ desafio_2/test.js
@@ -0,0 +1,42 @@
+const Container = require('./clases.js');
+
+//creamos la instancia de la clase container
+const products = new Container('products.txt');
+
+const main = async () => {
+  //Añadimos los productos
+  let product1 = await products.save({
+    title: 'Escuadra',
+    price: 123.45,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+  });
+  let product2 = await products.save({
+    title: 'Calculadora',
+    price: 234.56,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',
+  });
+  let product3 = await products.save({
+    title: 'Globo Terráqueo',
+    price: 345.67,
+    thumbnail:
+      'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',
+  });
+
+  //Buscamos producto por id 3
+  let productById = await products.getByID(3);
+  console.log('Producto:', productById);
+
+  //Eliminamos el producto con id 2
+  let deleteProduct = await products.deleteById(2);
+
+  //Traemos todos los productos
+  let allProducts = await products.getAll();
+  console.log('Todos los productos');
+  console.log(allProducts);
+
+  //Eliminamos el archivo con los productos.
+  let deleteAll = await products.deleteAll();
+};
+main();
