a579f8af5812b90eb91bf1998cb12a2eab48dd57
Cesar <camongez28@gmail.com>
2022-10-15 18:49:43 -0300

    desafio 4 finalizado

--- desafio_4/server.js
+++ desafio_4/server.js
@@ -38,6 +38,8 @@ app.use('/api/products', productosRouter);
 
 const PORT = 8080;
 const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+  console.log(
+    `Servidor http escuchando en el puerto ${server.address().port}.`
+  );
 });
 server.on('error', (error) => console.log(`Error en servidor ${error}`));
